2e06aa64ac2ce10341e9b36ff1f1b9a903c67327
Joseph <josephalainmendez@gmail.com>
2024-08-11 20:42:07 -0600

    Primer commit

---
+++ Persistencia-con-Spring/src/main/java/com/example/Persistencia_con_Spring/PersistenciaConSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.Persistencia_con_Spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersistenciaConSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersistenciaConSpringApplication.class, args);
+	}
+
+}
---
+++ Persistencia-con-Spring/src/test/java/com/example/Persistencia_con_Spring/PersistenciaConSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Persistencia_con_Spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersistenciaConSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
